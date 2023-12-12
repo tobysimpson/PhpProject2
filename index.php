3f80167b5adfb2f73086523fa3bca7b77eab43d3
Toby Simpson <toby.simpson@usi.ch>
2023-12-12 17:42:59 +0100

    testing with vetim more

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>hello kjhgfds</title>
+    </head>
+    <body>
+        <?php
+        echo 123;
+        ?>
+    </body>
+</html>
